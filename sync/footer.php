d6716cc156f29b0059229f097f9c8bd09bdd2d1f
noahkoenig <email@noahkoenig.de>
2023-02-19 23:11:06 +0100

    update socials, remove unused images

--- sync/footer.php
+++ sync/footer.php
@@ -1,14 +1,17 @@
 
 				<footer>
 					<a class="socialmedia" href="https://github.com/noahkoenig" target="_blank">
-						<img class="socialmediaicon" src="/src/icons/github.png">
+						<img class="socialmediaicon" src="/src/icons/github.webp">
+					</a>
+					<a class="socialmedia" href="https://www.linkedin.com/in/noah-k%C3%B6nig-02b244266/" target="_blank">
+						<img class="socialmediaicon" src="/src/icons/linkedin.webp">
 					</a>
 					<a class="socialmedia" href="https://www.youtube.com/@anwofilms" target="_blank">
 						<img class="socialmediaicon" src="/src/icons/youtube.webp">
 					</a>
-					<a class="socialmedia" href="https://www.instagram.com/noahkoen1g/" target="_blank">
+					<!--a class="socialmedia" href="https://www.instagram.com/noahkoen1g/" target="_blank">
 						<img class="socialmediaicon" src="/src/icons/instagram.png">
-					</a>
+					</a-->
 					<p class="calibriheaderfooter">
 						<a class="sitenotice" href="/site_notice.php">
 							Site Notice
@@ -24,6 +27,7 @@
 				</footer>
 
 				<script src="https://cdn.jsdelivr.net/npm/less"></script>
+				<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
 				<script>
 					<?php include($_SERVER['DOCUMENT_ROOT'] . '/js/global.js');?>
 				</script>
